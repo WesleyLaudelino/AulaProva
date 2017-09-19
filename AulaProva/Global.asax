95b16f058fdce503bb0b21b91fa7b815191d1e2b
Wesley Silva <wesleylaudelino@outlook.com>
2017-09-18 21:21:09 -0400

    Adicionar arquivos de projeto.

---
+++ AulaProva/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AulaProva.MvcApplication" Language="C#" %>
---
+++ AulaProva/Models/Estudante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AulaProva.Models
+{
+    public class Estudante
+    {
+        public int EstudanteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
